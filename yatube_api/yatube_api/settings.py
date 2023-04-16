5f2b4f1d85a2dc93dd10ddd2cc2a69e0b011dd6b
Николай Балыков <balykovnikolay13@gmail.com>
2023-04-16 17:59:52 +0700

    idk

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -92,6 +92,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
